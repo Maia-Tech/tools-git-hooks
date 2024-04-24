268e34d290b3c75ff1bbc69f36bb917822def8ca
Austin Cormier <austincormier@maia-tech.com>
2024-04-24 17:29:37 -0400

    Add prepare-commit-msg and the install_git_hooks.sh
    ENG-61 #time 30m

---
+++ install_git_hooks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the current directory is within a Git repository
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+  echo "Error: Not inside a Git repository."
+  exit 1
+fi
+
+git_top_level=$(git rev-parse --show-toplevel 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "ERROR: Could not extract the top of the workspace"
+    exit 1
+fi
+
+# Create a temporary directory
+temp_dir="$(mktemp -d)"
+
+# Clone the repository into the temporary directory
+git clone git@github.com:Maia-Tech/tools-git-hooks.git "$temp_dir"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to clone the tools-git-hooks repo"
+    exit 1
+fi
+
+# Copy the prepare-commit-msg hook file to the .git/hooks directory
+cp "$temp_dir/hooks/prepare-commit-msg" ".git/hooks/"
+
+# Make the hook file executable
+chmod +x ".git/hooks/prepare-commit-msg"
+
+# Clean up the temporary directory
+rm -rf "$temp_dir"
+
+echo "prepare-commit-msg hook installed successfully."
